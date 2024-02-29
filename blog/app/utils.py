fc5040fd75e12d4b1608e78f68b61cdc241e8385
Muruxo <83302154+Muruxo@users.noreply.github.com>
2024-02-29 17:38:20 -0500

    sidebar

--- blog/app/utils.py
+++ blog/app/utils.py
@@ -2,18 +2,31 @@ from .models import *
 import pandas as pd 
 
 
-def es_admin(user: User.is_staff):
-    if user == True:
+def es_admin(user): 
+    if hasattr(user, 'perfil_admin'):
         return True 
+    else: 
+        return False
+    
+def es_admin(user): 
+    if hasattr(user, 'perfil_postulante'):
+        return True 
+    else: 
+        return False
+
+
+# def es_admin(user: User.is_staff):
+#     if user == True:
+#         return True 
     
 
-def es_postulante(user: User.is_staff):
-    if user == False:
-        return False 
+# def es_postulante(user: User.is_staff):
+#     if user == False:
+#         return False 
     
 
-def tiene_cuenta(user: User.is_authenticated):
-    if user == False:
-        return
+# def tiene_cuenta(user: User.is_authenticated):
+#     if user == False:
+#         return
    
     
