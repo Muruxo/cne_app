102441294dd94fa3aa935c549f1465fe52e0769a
anqx77 <adrianqx77@hotmail.com>
2024-03-11 22:55:05 -0500

    asdasd

--- blog/app/utils.py
+++ blog/app/utils.py
@@ -2,18 +2,11 @@ from .models import *
 import pandas as pd 
 
 
-def es_admin(user): 
-    if hasattr(user, 'perfil_admin'):
-        return True 
-    else: 
-        return False
-    
-def es_postulante(user): 
-    if hasattr(user, 'perfil_postulante'):
-        return True 
-    else: 
-        return False
+def es_admin(user):
+    return hasattr(user, 'perfil_admin')
 
+def es_postulante(user):
+    return hasattr(user, 'perfil_postulante')
 
 # def es_admin(user: User.is_staff):
 #     if user == True:
